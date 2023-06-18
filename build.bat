a4e80d68060b913f1805a59082e7731c66a07d53
XXL Steve <80220423+CornetPanique86@users.noreply.github.com>
2023-06-18 13:02:21 +0200

    Integrity attribute restored (test)

--- build.bat
+++ build.bat
@@ -1,3 +1,1 @@
-echo "Starting build with args"
-hugo -b https://xxlsteve.net -E -F --minify --ignoreCache
-echo "Build ended in /public"
+hugo -b https://xxlsteve.net -E -F --minify --ignoreCache --verbose
